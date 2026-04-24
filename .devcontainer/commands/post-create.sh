6746502cc077617a4ba5a3db6c8eace7137246f4
orlowskilp <lukasz@orlowski.io>
2026-04-24 11:24:29 +0800

    chore(devcontainer): Added some tools to devcontainer

--- .devcontainer/commands/post-create.sh
+++ .devcontainer/commands/post-create.sh
@@ -7,7 +7,7 @@ git config --global --add safe.directory $(pwd)
 # Suspend Git's message about moving to `main` as the default branch name
 git config --global init.defaultBranch master
 
-echo "if [ ! -f ${CONTAINER_WORKSPACE_FOLDER}/.envrc ]; then touch ${CONTAINER_WORKSPACE_FOLDER}/.envrc; fi" >> ~/.bashrc
+echo -e "\nif [ ! -f ${CONTAINER_WORKSPACE_FOLDER}/.envrc ]; then touch ${CONTAINER_WORKSPACE_FOLDER}/.envrc; fi" >> ~/.bashrc
 
 # Allow direnv to load environment variables every time a new shell is started
 echo -e "\ndirenv allow ${CONTAINER_WORKSPACE_FOLDER}" >> ~/.bashrc
